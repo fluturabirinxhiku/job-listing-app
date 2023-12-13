54206f34ba4a4e551e7118c39ccc6a6930966435
Flutura Birinxhiku <fluturabirinxhiku@gmail.com>
2023-12-13 22:43:16 +0100

    Split UI into partials

--- helpers.php
+++ helpers.php
@@ -10,3 +10,37 @@ function basePath($path = '')
 {
     return __DIR__ . '/' . $path;
 }
+
+/**
+ * Load a view
+ * 
+ * @param string $name
+ * @param array $data
+ * @return void 
+ */
+function loadView($name, $data = [])
+{
+    $viewPath = basePath("views/{$name}.view.php");
+    if (file_exists($viewPath)) {
+        extract($data);
+        require $viewPath;
+    } else {
+        echo "View {$name} not found";
+    }
+}
+
+/**
+ * Load a partial
+ * 
+ * @param string $name
+ * @return void 
+ */
+function loadPartial($name)
+{
+    $partialPath = basePath("views/partials/{$name}.php");
+    if (file_exists($partialPath)) {
+        require $partialPath;
+    } else {
+        echo "Partial {$name} not found";
+    }
+}
