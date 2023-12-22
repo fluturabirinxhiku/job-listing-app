34444507b00d986d8876802f55ac421362b4ef0b
Flutura Birinxhiku <fluturabirinxhiku@gmail.com>
2023-12-22 13:10:32 +0100

    Create error view and controller

---
+++ App/controllers/ErrorController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controllers;
+
+use Framework\Database;
+
+class ErrorController
+{
+    /**
+     * Not found error
+     *
+     * @param string $message
+     * @return void
+     */
+    public static function notFound($message = 'Resource not found')
+    {
+        http_response_code(404);
+
+        loadView('error', ['status' => '404',
+            'message' => $message]);
+    }
+    /**
+     *  Unauthorized error
+     *
+     * @param string $message
+     * @return void
+     */
+    public static function unauthorized($message = 'You are not authorized to view this resource')
+    {
+        http_response_code(403);
+
+        loadView('error', ['status' => '403',
+            'message' => $message]);
+    }
+}
